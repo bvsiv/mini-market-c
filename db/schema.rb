fa0e5695e46c9bce0b3a80c48b9b842a3bd50c72
Agnieszka Dunin <agnieszka.dunin@gmail.com>
2018-05-09 19:20:31 +0200

    Add status column with proposed as default

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_09_170245) do
+ActiveRecord::Schema.define(version: 2018_05_09_171451) do
 
   create_table "offers", force: :cascade do |t|
     t.decimal "suggested_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "status", default: "proposed"
   end
 
 end

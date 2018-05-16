b3dfb35bdf0a2ebfd6df91989d84b366239bd414
Basia Kaźmierczak <baska.kazmierczak@gmail.com>
2018-05-16 10:03:14 +0200

    Move merchant presence validation to association in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,5 @@
 class Product < ApplicationRecord
   validates :name, presence: true
   validates :description, presence: true
-  belongs_to :merchant
-  validates :merchant, presence: true
+  belongs_to :merchant, required: true
 end

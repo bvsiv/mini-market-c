1752694eb6872f2287cb988c6909df2e3683fcc6
Agnieszka Dunin <agnieszka.dunin@gmail.com>
2018-05-16 17:32:14 +0200

    Add customer column to offer model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,4 +1,6 @@
 class Offer < ApplicationRecord
   validates :suggested_price, presence: true
   validates :status, presence: true
+  belongs_to: customer
+  validates :customer, presence: true
 end

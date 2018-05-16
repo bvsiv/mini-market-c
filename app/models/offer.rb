41052868def3e0b0286bb85222d82df7d4ac17af
Agnieszka Dunin <agnieszka.dunin@gmail.com>
2018-05-16 17:35:55 +0200

    Add merchant column to offer model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,6 +1,8 @@
 class Offer < ApplicationRecord
   validates :suggested_price, presence: true
   validates :status, presence: true
-  belongs_to: customer
+  belongs_to :customer
   validates :customer, presence: true
+  belongs_to :merchant
+  validates :merchant, presence: true
 end

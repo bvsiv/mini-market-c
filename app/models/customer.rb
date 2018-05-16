d83a03f8d8632fdcf77216d73915b3de1864d5e8
justynaaleksandra <justyna.oleksiak@gmail.com>
2018-05-16 20:18:06 +0200

    spaces finally set

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,5 +2,5 @@ class Customer < ApplicationRecord
 	validates :name, presence: true
 	validates :email, presence: true, format: /\A.+@.+\..+\z/, uniqueness: true
 
-		has_many :offers
+	has_many :offers
 end

e55bba678e1aadcaf4f69f06e255e1c002ce23a6
justynaaleksandra <justyna.oleksiak@gmail.com>
2018-05-18 00:17:15 +0200

    defined methods current_customer and require_customer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :current_customer
+
+  def current_customer
+    @current_customer ||= Customer.find(session[:customer_id]) if session[:customer_id] 
+  end
+
+  def require_customer 
+    redirect_to '/login' unless current_customer
+  end
+
 end

462514363a95ecc6a88ce69fd197ede71634a843
justynaaleksandra <justyna.oleksiak@gmail.com>
2018-05-17 20:56:57 +0200

    Sessions controller generated, lo gi form ready

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'signup'  => 'customers#new' 
   resources :customers
+
+  get 'login'  => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 end

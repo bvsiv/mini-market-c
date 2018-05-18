b96ecc31707dffe02584019bf0b1e024bf6bb66b
Basia Kaźmierczak <baska.kazmierczak@gmail.com>
2018-05-18 18:34:29 +0200

    Add controllers for merchants and sessions

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'dashboard#show'
+
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+  resources :sessions, only: [:create]
+  resources :merchants, only: [:new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

e67eea5fbea47315a94b2e428810ae43d84de6fc
justynaaleksandra <justyna.oleksiak@gmail.com>
2018-05-21 19:35:20 +0200

    namespace added

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'signup'  => 'customers#new' 
-  resources :customers
+  namespace :customers do
+    get 'signup'  => 'customers#new' 
+    resources :customers
 
-  get 'login'  => 'sessions#new'
-  post 'login' => 'sessions#create'
-  delete 'logout' => 'sessions#destroy'
+    get 'login'  => 'sessions#new'
+    post 'login' => 'sessions#create'
+    delete 'logout' => 'sessions#destroy'
+  end
 end

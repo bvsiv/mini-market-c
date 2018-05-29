f480281b40d9534686ed55e002d9cf029bca488c
justynaaleksandra <justyna.oleksiak@gmail.com>
2018-05-29 14:39:49 +0200

    offer model fixed

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,5 +2,5 @@ class Offer < ApplicationRecord
   validates :suggested_price, presence: true
   validates :status, presence: true
   belongs_to :customer, required: true
-  belongs_to :merchant, required: true
+  belongs_to :product, required: true
 end

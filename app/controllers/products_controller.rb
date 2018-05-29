a553f12fa78a7df6ebba07ff7c1e9a22942a3ff8
justynaaleksandra <justyna.oleksiak@gmail.com>
2018-05-29 14:29:40 +0200

    showing products names fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -11,7 +11,7 @@ class ProductsController < ApplicationController
 
   private 
   def product_params 
-    params.require(:product).permit(:name, :description, :suggested_price) 
+    params.require(:product).permit(:name, :description,) 
   end
 
 end

4be1b6f0b46332e585ebcd79bd4e5ada5a391909
justynaaleksandra <justyna.oleksiak@gmail.com>
2018-06-29 16:37:18 +0200

    creating a new offer works

--- app/controllers/customers/offers_controller.rb
+++ app/controllers/customers/offers_controller.rb
@@ -11,14 +11,19 @@ module Customers
     def create
       @customer = current_customer
       @product=Product.find(params[:id])
+
       @offer = Offer.new(offer_params)
       if @offer.save
-        offer[:suggested_price] = @offer.suggested_price
-        redirect_to customers_products_path(@customer.id)
+        @offer[:suggested_price] = @offer.suggested_price
+        redirect_to customers_product_details_path(:id => @product.id)
      else
-        redirect_to customers_products_new_offer_path(:id => product.id)
+        redirect_to customers_products_new_offer_path(:id => @product.id)
       end
     end
 
+    private
+      def offer_params
+        params.require(:offer).permit(:suggested_price).merge(status: "proposed", customer_id: current_customer.id, product_id: @product.id)
+      end
   end
 end
